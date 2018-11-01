18c9842f3959ebcb791a04c702ead0565902fb5b
Chris Chateau <christopherchateau@gmail.com>
2018-11-01 08:35:30 -0600

    Add styling/spacing to grid areas in CharacterProfile

--- src/containers/CharacterProfile/index.js
+++ src/containers/CharacterProfile/index.js
@@ -2,19 +2,24 @@ import React from "react";
 import { connect } from "react-redux";
 import "./CharacterProfile.css";
 
-const CharacterProfile = (props) => {
-
-  const { pic } = props.storedCharacters
-  //console.log(props.storedCharacters[1])
+const CharacterProfile = props => {
   return (
-  <div className="CharacterProfile">
-    <img className="picture" src={pic}/>
-    <article className="description">
-
-    </article>
-    <section className="comics">
-    </section>
-  </div>) 
+    <div className="CharacterProfile">
+      <section className="profile-wrapper">
+        <img
+          className="picture"
+          src="https://pbs.twimg.com/profile_images/847820097821581312/gpfF5-xH_400x400.jpg"
+        />
+        <article className="description">
+          <p>
+            Spiderman Spiderman Spiderman Spiderman Spiderman Spiderman
+            Spiderman Spiderman Spiderman Spiderman Spiderman
+          </p>
+        </article>
+        <article className="comics">comics comics comics comics comics</article>
+      </section>
+    </div>
+  );
 };
 
 export const mapStateToProps = state => ({
