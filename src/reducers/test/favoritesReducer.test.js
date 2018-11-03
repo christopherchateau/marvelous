66a3e3c85ef17f3c8a058e960458375f73b3313b
Chris Chateau <christopherchateau@gmail.com>
2018-11-03 13:50:31 -0600

    Write tests for FavoritesReducer

---
+++ src/reducers/test/favoritesReducer.test.js
@@ -0,0 +1,23 @@
+import { favoritesReducer } from "../favoritesReducer";
+
+describe("favoritesReducer", () => {
+  it("should return the initial state", () => {
+    const expected = false;
+    const result = favoritesReducer(undefined, {});
+    expect(result).toEqual(expected);
+  });
+
+  it("should toggle showFavorites boolean", () => {
+    const action = {
+      type: "SHOW_FAVORITES"
+    };
+
+    let mockState = false;
+    let result = favoritesReducer(mockState, action);
+    expect(result).toEqual(true);
+
+    mockState = true;
+    result = favoritesReducer(mockState, action);
+    expect(result).toEqual(false);
+  });
+});
